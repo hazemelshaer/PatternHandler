6d54ae479f3885f4675dff07a2d6ac5768efe465
Hazem Elshaer <hazem.adel.elshaer@gmail.com>
2024-04-18 21:23:04 +0200

    Adding unit tests using gtest for Utils file

---
+++ src/tst/Utils_test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+// #include <gmock/gmock.h>
+#include "Utils.hpp"  // Include your main classes header
+using namespace Utils;
+TEST(UtilsTest, IsCalledToString) {
+    EXPECT_EQ(isCalledToString(true), "true");
+    EXPECT_EQ(isCalledToString(false), "false");
+}
+
+// Test for converting string to boolean
+TEST(UtilsTest, IsCalledToBool) {
+    EXPECT_TRUE(Utils::isCalledToBool("true"));
+    EXPECT_TRUE(Utils::isCalledToBool("True"));
+    EXPECT_FALSE(Utils::isCalledToBool("false"));
+    EXPECT_FALSE(Utils::isCalledToBool("anyOtherValue"));
+}
+
+// Test for printing patterns (mocking std::cout)
+TEST(UtilsTest, PrintPatterns) {
+    std::vector<PatternCall> patterns = {
+        {1, "Pattern1", "file1.pat", true},
+        {2, "Pattern2", "file2.pat", false}
+    };
+
+    testing::internal::CaptureStdout();  // Start capturing stdout
+    Utils::printPatterns(patterns);
+    std::string output = testing::internal::GetCapturedStdout();  // Stop capturing stdout
+
+    std::string expectedOutput =
+        "1,Pattern1,file1.pat,1\n"
+        "2,Pattern2,file2.pat,0\n";
+    EXPECT_EQ(output, expectedOutput);
+}
+
---
+++ src/tst/test_main.cpp
@@ -0,0 +1,14 @@
+/**
+*  @file main.cpp
+* @author Hazem Elshaer
+* @brief 
+* @date 2024-04-18
+* 
+* @copyright Copyright (c) 2024
+*/
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
